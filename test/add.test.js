b3e27b3f0cd3dfde9286387993c1224de355c547
Ankit <ankit@Ankits-MacBook-Air.local>
2024-08-22 22:23:03 +0530

    Initial commit

---
+++ test/add.test.js
@@ -0,0 +1,10 @@
+const add = require('../stringCalculator');
+
+// Test cases
+describe('add(numbers)', () => {
+
+  test('should return 0 for an empty string', () => {
+    expect(add("")).toBe(0);
+  });
+
+});
